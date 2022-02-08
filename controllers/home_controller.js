1eb3f8a334cc13765bd470504f89c94e201e05c1
Priyanshu <kumarpriyanshu2822@gmail.com>
2022-02-08 23:20:48 +0530

    check authentication on each post

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -4,7 +4,7 @@ module.exports.home = function(req, res){
     // console.log(req.cookies);
     // res.cookie('user_id', 25);
     
-
+    //populate the user of each post
     Post.find({}).populate('user').exec(function(err,posts){
         return res.render('home', {
             title: "Codeial | Home",
